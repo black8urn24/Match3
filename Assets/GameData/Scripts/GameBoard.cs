1ae982c962dfece5f9e50b1d5f674a0df32dbb8f
Sharath <sharathofreak80@gmail.com>
2021-12-27 19:07:12 +0530

    Added dynamic camera placement for game boards

--- Assets/GameData/Scripts/GameBoard.cs
+++ Assets/GameData/Scripts/GameBoard.cs
@@ -10,6 +10,7 @@ namespace Match3.Core
         [SerializeField] private GameObject gameTilePrefab = null;
         [SerializeField] private int boardWidth = -1;
         [SerializeField] private int boardHeight = -1;
+        [SerializeField] private float borderSize = -1f;
         #endregion
 
         #region Variables
@@ -43,16 +44,28 @@ namespace Match3.Core
                     tile.transform.rotation = Quaternion.identity;
                     tile.transform.name = "Tile[" + i + "," + j + "]";
                     GameTile gameTile = tile.GetComponent<GameTile>();
-                    if(gameTile != null)
+                    if (gameTile != null)
                     {
                         allTiles[i, j] = gameTile;
                     }
                 }
             }
-            #endregion
-
-            #region Public Methods
-            #endregion
+            SetCameraDimensions();
         }
+
+        private void SetCameraDimensions()
+        {
+            Camera mainCamera = Camera.main;
+            Vector3 finalCameraPosition = new Vector3((float)(boardWidth - 1) / 2f, (float)(boardHeight - 1) / 2f, -10f);
+            mainCamera.transform.position = finalCameraPosition;
+            float aspectRatio = (float)Screen.width / (float)Screen.height;
+            float verticalSize = (float)boardHeight / 2f + borderSize;
+            float horizontalSize = ((float)boardWidth / 2f + borderSize) / aspectRatio;
+            mainCamera.orthographicSize = (verticalSize > horizontalSize) ? verticalSize : horizontalSize;
+        }
+        #endregion
+
+        #region Public Methods
+        #endregion
     }
 }
