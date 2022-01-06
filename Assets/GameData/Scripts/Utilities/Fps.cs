f7572add24aaceabc0ce2406b6a22140034e4f42
Sharath <sharathofreak80@gmail.com>
2022-01-06 09:53:22 +0530

    Added fps counter to main scene

---
+++ Assets/GameData/Scripts/Utilities/Fps.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Match3.Utilities
+{
+    public class Fps : MonoBehaviour
+    {
+        #region Variables
+        private string label = "";
+		private float count;
+        #endregion
+
+        #region Unity Methods
+        IEnumerator Start()
+		{
+			GUI.depth = 2;
+			while (true)
+			{
+				if (Time.timeScale == 1)
+				{
+					yield return new WaitForSeconds(0.1f);
+					count = (1 / Time.deltaTime);
+					label = "FPS :" + (Mathf.Round(count));
+				}
+				else
+				{
+					label = "Pause";
+				}
+				yield return new WaitForSeconds(0.5f);
+			}
+		}
+
+		void OnGUI()
+		{
+			GUIStyle style = new GUIStyle();
+			style.fontSize = 45;
+			GUI.Label(new Rect(10, 40, 100, 100), label.ToAqua().ToBold(), style);
+		}
+        #endregion
+    }
+}
