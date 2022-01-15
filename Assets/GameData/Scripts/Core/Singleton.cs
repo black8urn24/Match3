30dbf6cb259d10d08038f90ccdc845a19960bdff
Sharath <sharathofreak80@gmail.com>
2022-01-15 22:30:38 +0530

    Added generic Singleton Component

---
+++ Assets/GameData/Scripts/Core/Singleton.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Match3.Core
+{
+    public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+    {
+        #region SingleTon
+        private static T instance;
+        #endregion
+
+        #region Properties
+        public static T Instance
+        {
+            get
+            {
+                if(instance == null)
+                {
+                    instance = GameObject.FindObjectOfType<T>();
+                    if(instance == null)
+                    {
+                        GameObject singleton = new GameObject(typeof(T).Name);
+                        instance = singleton.AddComponent<T>();
+                    }
+                }
+                return instance;
+            }
+        }
+        #endregion
+
+        #region Unity Methods
+        public virtual void Awake()
+        {
+            if(instance == null)
+            {
+                instance = this as T;
+                DontDestroyOnLoad(this.gameObject);
+            }
+            else
+            {
+                Destroy(gameObject);
+            }
+        }
+        #endregion
+    }
+}
