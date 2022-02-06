a3c3f000d28ef24335dc17100f6d67a3c003a00d
Sharath <sharathofreak80@gmail.com>
2022-02-06 12:03:13 +0530

    Added visual effects for gamepieces for timebonus

--- Assets/GameData/Scripts/Core/TimeBonus.cs
+++ Assets/GameData/Scripts/Core/TimeBonus.cs
@@ -8,11 +8,40 @@ namespace Match3.Core
     public class TimeBonus : MonoBehaviour
     {
         #region Inspector Variables
-        [SerializeField] private int bonusValue = 2;
+        [SerializeField] [Range(1f, 5f)] private int bonusValue = 2;
+        [SerializeField] [Range(0f, 1f)] private float chanceForBonus = 0.1f;
+        [SerializeField] private GameObject bonusGlow = null;
+        [SerializeField] private GameObject ringGlow = null;
         #endregion
 
         #region Properties
         public int BonusValue { get => bonusValue; private set => bonusValue = value; }
+        #endregion
+
+        #region Unity Methods
+        private void Start()
+        {
+            float random = Random.Range(0f, 1f);
+            if(random > chanceForBonus)
+            {
+                BonusValue = 0;
+            }
+            SetState(BonusValue != 0);
+        }
+        #endregion
+
+        #region Private Methods
+        private void SetState(bool state)
+        {
+            if(bonusGlow != null)
+            {
+                bonusGlow.SetActive(state);
+            }
+            if(ringGlow != null)
+            {
+                ringGlow.SetActive(state);
+            }
+        }
         #endregion
     }
 }
